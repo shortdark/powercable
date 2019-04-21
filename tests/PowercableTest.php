2233286dec344a37aadad939451bd4b18b20bf1e
neil <neil@wrm-media.com>
2019-04-21 18:46:30 +0100

    PHP Unit

---
+++ tests/PowercableTest.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class PowercableTest extends TestCase
+{
+
+    public function testCanBeUsedAsString()
+    {
+        $this->assertEquals(
+            1556409600,
+            Powercable::resetDateToMidnight(1556454600)
+        );
+    }
+}
